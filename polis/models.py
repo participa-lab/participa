f3b22e005bdcb494ae8106ddf5da505f9d720858
raúl speroni <raulsperoni@gmail.com>
2024-03-16 16:00:33 -0300

    good!

--- polis/models.py
+++ polis/models.py
@@ -1,10 +1,13 @@
-from django.db import models
-from polis import choices
 import uuid
+
+from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+from polis import choices
 
 
 class Instance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
 

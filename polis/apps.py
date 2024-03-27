78f9e9e4333c432a405a6c4d7172a1ea6db0dd53
raúl speroni <raulsperoni@gmail.com>
2024-03-27 15:32:42 -0300

    signals

--- polis/apps.py
+++ polis/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class PolisConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "polis"
+
+    def ready(self):
+        import polis.signals
+
+        print(polis.signals.__name__)
--- polis/signals.py
+++ polis/signals.py
@@ -2,11 +2,11 @@ import hashlib
 
 from allauth.account.signals import user_signed_up
 from django.dispatch import receiver
-from models import Participant
+from polis.models import Participant
 
 
 @receiver(user_signed_up)
-def social_login_fname_lname_profilepic(sociallogin, user):
+def social_login_fname_lname_profilepic(sociallogin, user, **kwargs):
     preferred_avatar_size_pixels = 256
 
     picture_url = "http://www.gravatar.com/avatar/{0}?s={1}".format(

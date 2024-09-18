85215a956509bcca3fa6fb009751658fbee30c5c
raúl speroni <raulsperoni@gmail.com>
2024-09-18 13:06:59 -0300

    progress

--- theme/static/js/main.js
+++ theme/static/js/main.js
@@ -1,8 +1,8 @@
 import { menu } from "./menu.js";
-//import { slider } from "./slider.js";
+import { slider } from "./slider.js";
 import { Tabs } from "./tabs.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   menu();
-  //slider();
+  slider();
 });
--- theme/static/js/slider.js
+++ theme/static/js/slider.js
@@ -1,11 +1,11 @@
-//import Splide from "@splidejs/splide";
 
-//const slider = () => {
- // var elms = document.getElementsByClassName("splide");
-
-//  for (var i = 0; i < elms.length; i++) {
- //   new Splide(elms[i]).mount();
-//  }
-////};
-
-//export { slider };
+const slider = () => {
+    var elms = document.getElementsByClassName("splide");
+  
+    for (var i = 0; i < elms.length; i++) {
+      new Splide(elms[i]).mount();
+    }
+  };
+  
+  export { slider };
+  
